265e2a523feaf013c715b91b8146992aa3e59c8a
Wenderson Aquino <wensantos.aquino@gmail.com>
2024-10-04 21:38:58 -0300

    Add files via upload

---
+++ SistemaInformacao/Produto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInformacao
+{
+    internal class Produto
+    {
+        int id;
+        string descricao;
+        double valor;
+
+        public int Id { get => id; set => id = value; }
+
+        public Produto(int id, string descricao, double valor)
+        {
+            this.descricao = descricao;
+            this.valor = valor;
+            this.id = id;
+        }
+
+        public void ReajusteValor(double percentual)
+        {
+            this.valor += this.valor * percentual / 100;
+            Console.WriteLine(this.valor);
+        }
+
+        public void MostrarDados()
+        {
+            Console.WriteLine("Descrição " + descricao);
+            Console.WriteLine("Valor: R$" + valor.ToString("C2"));
+        }
+
+        ~Produto() { }
+    }
+}
---
+++ SistemaInformacao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInformacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto arroz = new Produto(1, "Arroz Sepé - Tipo 1", 6.45);
+            arroz.ReajusteValor(5);
+            Estoque estoque = new Estoque();
+            estoque.AdicionarProduto(arroz);
+            arroz.MostrarDados();
+            Console.ReadKey();
+        }
+    }
+}
